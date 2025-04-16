7b06ae3b516aa761e402eda529e4fec0f1117b71
Cristian Florez <ingenieroflorezco@gmail.com>
2025-04-16 17:19:55 -0500

    chore(cli): remove meow as CLI flags are not being used

--- source/cli.tsx
+++ source/cli.tsx
@@ -3,26 +3,4 @@ import React from 'react';
 import {render} from 'ink';
 import App from './App.js';
 
-// const cli = meow(
-// 	`
-// 	Usage
-// 	  $ dashit-navi
-
-// 	Options
-// 		--name  Your name
-
-// 	Examples
-// 	  $ dashit-navi --name=Jane
-// 	  Hello, Jane
-// `,
-// 	{
-// 		importMeta: import.meta,
-// 		flags: {
-// 			name: {
-// 				type: 'string',
-// 			},
-// 		},
-// 	},
-// );
-
 render(<App />);
